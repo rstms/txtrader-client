9de1a1aa3772dc62345a3f354780166caa66cb49
Matt Krueger <mkrueger@rstms.net>
2020-07-02 02:52:21 -0500

    bumped version to 1.3.2

--- txtrader_client/__version__.py
+++ txtrader_client/__version__.py
@@ -1,16 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-  version.py
-  ----------
-
-  txTraderClient version module 
-
-  Copyright (c) 2017 Reliance Systems Inc. <mkrueger@rstms.net>
-  Licensed under the MIT license.  See LICENSE for details.
-
-"""
-
-NAME = 'txtrader_client'
-DESCRIPTION = 'TxTrader Securities Trading API Client'
-__version__ = '1.2.0'
+DATE='2020-07-02'
+TIME='02:52:21'
+VERSION='1.3.2'
